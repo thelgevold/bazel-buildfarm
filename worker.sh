a2af4a1935d0b14b5c91d3417cf7f9a6a6dcce2c
tor <torgeirh@hotmail.com>
2018-07-08 15:17:47 -0400

    remove isolated workers

--- worker.sh
+++ worker.sh
@@ -1,9 +1,1 @@
-bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config --root worker1 &
-bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config --root worker2 &
-bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config --root worker3 &
-bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config --root worker4 &
-bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config --root worker5 &
-bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config --root worker6 &
-
-wait
-echo all processes complete
+bazel build //src/main/java/build/buildfarm:buildfarm-worker && bazel-bin/src/main/java/build/buildfarm/buildfarm-worker config/worker.config
